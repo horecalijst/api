44c4a8ed553d1e510b5483cbbaa945ff55aa8a86
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-25 15:22:23 +0200

    :sparkles: Fallback to btw-opzoeken.be

--- src/graphql/business/query.ts
+++ src/graphql/business/query.ts
@@ -1,7 +1,7 @@
 import { GraphqlContext } from 'server';
 import validateVat, { CountryCodes } from 'validate-vat-ts';
 
-const vatLookup = (
+const vatLookup = async (
   _parent: any,
   { vat }: { vat: string },
   { user }: GraphqlContext,
@@ -10,9 +10,58 @@ const vatLookup = (
     throw new Error('not authenticated');
   }
 
-  const cleanedVat = vat.replace(/[^0-9]+/g, '');
+  const vatNumber = vat.replace(/[^0-9]+/g, '');
 
-  return validateVat(CountryCodes.Belgium, cleanedVat);
+  try {
+    const viesResponse = await validateVat(CountryCodes.Belgium, vatNumber);
+
+    if (!viesResponse?.name) {
+      return null;
+    }
+
+    if (viesResponse?.name === '---') {
+      return null;
+    }
+
+    return viesResponse;
+  } catch {
+    // noop
+  }
+
+  // fallback to third party with possibly outdated data
+  try {
+    const response = await fetch(
+      `https://www.btw-opzoeken.be/VATSearch/Search?KeyWord=${vatNumber}`,
+    );
+
+    const json = await response.json();
+    if (!Array.isArray(json)) {
+      return null;
+    }
+
+    if (json.length === 0) {
+      return null;
+    }
+
+    const data = json.shift();
+    if (!data?.CompanyName) {
+      return null;
+    }
+
+    return {
+      countryCode: 'BE',
+      vatNumber,
+      valid: data?.JuridicalSituation === 'Normale toestand',
+      name: data?.CompanyName,
+      address: `${data?.Street} ${data?.StreetNumber}${
+        data?.Box ? `/${data?.Box}` : ''
+      }, ${data?.Zipcode} ${data?.City}`,
+    };
+  } catch (e) {
+    // noop
+  }
+
+  return null;
 };
 
 export default {
