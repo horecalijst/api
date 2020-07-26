534b0b3373133491cbe402fc479b501c8f1534d5
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-26 20:02:11 +0200

    :card_file_box: Orders

---
+++ src/models/order.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from 'services/sequelize';
+
+export enum OrderStatus {
+  Pending = 'PENDING',
+  Paid = 'PAID',
+  Canceled = 'CANCELED',
+}
+
+class Order extends Model {
+  public id!: string;
+  public userId!: string;
+  public businessId!: string;
+  public productId!: string;
+  public externalReference!: string;
+  public status!: OrderStatus;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt?: Date | null;
+}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    businessId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    externalReference: { type: DataTypes.STRING(16), allowNull: false },
+    status: {
+      type: DataTypes.ENUM(
+        OrderStatus.Pending,
+        OrderStatus.Paid,
+        OrderStatus.Canceled,
+      ),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'orders',
+    timestamps: true,
+    paranoid: true,
+  },
+);
+
+export default Order;
