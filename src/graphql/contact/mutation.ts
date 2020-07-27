f2cbb0b4dff8b793d198f95940542b0b36650854
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-27 12:10:30 +0200

    :bug: No auth needed

--- src/graphql/contact/mutation.ts
+++ src/graphql/contact/mutation.ts
@@ -1,6 +1,5 @@
 import Business from 'models/business';
 import Contact from 'models/contact';
-import { GraphqlContext } from 'server';
 
 const addContact = async (
   _parent: any,
@@ -15,12 +14,7 @@ const addContact = async (
     phone: string | null;
     businessId: string;
   },
-  { user }: GraphqlContext,
 ) => {
-  if (!user) {
-    throw new Error('not authenticated');
-  }
-
   if (!email && !phone) {
     throw new Error('both email & phone are missing');
   }
