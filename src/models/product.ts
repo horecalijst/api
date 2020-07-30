488fda2bf105327cfa2a6bdd43cd54cf27c6ea13
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-30 15:03:25 +0200

    :art: Re-order some things

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,23 +1,23 @@
 import { DataTypes, Model } from 'sequelize';
 import sequelize from 'services/sequelize';
 
-export enum ProductCurrency {
-  Euro = 'EUR',
-}
-
 export enum ProductPeriod {
   Monthly = 'MONTHLY',
   Quarterly = 'QUARTERLY',
   Yearly = 'YEARLY',
 }
 
+export enum ProductCurrency {
+  Euro = 'EUR',
+}
+
 class Product extends Model {
   public id!: string;
   public description!: string;
+  public period!: ProductPeriod;
+  public time!: number;
   public currency!: ProductCurrency;
   public value!: number;
-  public time!: number;
-  public period!: ProductPeriod;
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
   public readonly deletedAt?: Date | null;
@@ -31,9 +31,6 @@ Product.init(
       primaryKey: true,
     },
     description: { type: DataTypes.STRING(16), allowNull: false },
-    currency: { type: DataTypes.ENUM(ProductCurrency.Euro), allowNull: false },
-    value: { type: DataTypes.FLOAT(8, 2), allowNull: false },
-    time: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
     period: {
       type: DataTypes.ENUM(
         ProductPeriod.Monthly,
@@ -42,6 +39,9 @@ Product.init(
       ),
       allowNull: false,
     },
+    time: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+    currency: { type: DataTypes.ENUM(ProductCurrency.Euro), allowNull: false },
+    value: { type: DataTypes.FLOAT(8, 2), allowNull: false },
   },
   {
     sequelize,
