373344daff4f48b1cb1978ff0f5ce08152554579
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-31 13:04:08 +0200

    :sparkles: Destroy expired contacts

---
+++ src/scripts/destroy-expired-contacts.ts
@@ -0,0 +1,33 @@
+import { subDays } from 'date-fns';
+import hasha from 'hasha';
+import Contact from 'models/contact';
+import { Op } from 'sequelize';
+
+(async () => {
+  const expiredContactIds = (
+    await Contact.findAll({
+      attributes: ['id'],
+      where: {
+        createdAt: {
+          [Op.lte]: subDays(new Date(), 14),
+        },
+        destroyedAt: null,
+      },
+    })
+  ).map((expiredContact) => expiredContact.id);
+
+  console.log(`Found ${expiredContactIds.length} expired contacts`);
+
+  for (const id of expiredContactIds) {
+    console.log(`Destroying data for ${id}`);
+
+    const contact = (await Contact.findOne({ where: { id } })) as Contact;
+    contact.name = hasha(`${contact.name}`, { algorithm: 'md5' });
+    contact.phone = hasha(`${contact.phone}`, { algorithm: 'md5' });
+    contact.email = hasha(`${contact.email}`, { algorithm: 'md5' });
+    contact.destroyedAt = new Date();
+    await contact.save();
+  }
+
+  process.exit(0);
+})();
